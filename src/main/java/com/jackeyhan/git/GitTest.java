fe91ae5ab3b333d678da96eefbe31dd23bcbbe00
Jackeyhan-JH <294773758@qq.com>
2022-03-20 14:12:34 +0800

    first commit

---
+++ src/main/java/com/jackeyhan/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.jackeyhan.git;
+
+/**
+ * @author Jackeyhan
+ * @ClassName GitTest
+ * @Description testGit
+ * @date 2022/3/20 14:09
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
