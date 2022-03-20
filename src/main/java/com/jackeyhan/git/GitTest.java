7c6c19a5fc3a510a74cca19b97ce84efa2469a64
Jackeyhan-JH <75495282+Jackeyhan-JH@users.noreply.github.com>
2022-03-20 14:45:40 +0800

    pull commit

--- src/main/java/com/jackeyhan/git/GitTest.java
+++ src/main/java/com/jackeyhan/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("Hot-fix test!");
         System.out.println("master test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
